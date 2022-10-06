3ddbf80b8b7f0b91af42b1234e5775cf1529ddcb
Artem <artp80@gmail.com>
2022-10-06 18:55:43 +0400

    Поправил команды, ns для старта nodemon

--- shop-bot/index.js
+++ shop-bot/index.js
@@ -1,10 +1,5 @@
+const validSecret = require("./utils/devops")
 const token = process.env["TOKEN"] || null
-try{
-    if(!token){
-        throw new Error("Токен не найден!")
-    }
-    console.log(token)
-}
-catch (err){
-    console.log(err)
-}
+validSecret(token)
+
+
---
+++ shop-bot/utils/devops.js
@@ -0,0 +1,13 @@
+const validSecret = (token)=>{
+    try{
+        if(!token){
+            throw new Error("Токен не найден!")
+        }
+        console.log(token)
+    }
+    catch (err){
+        console.log(err)
+    }
+}
+
+module.exports = validSecret
