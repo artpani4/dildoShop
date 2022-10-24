b26f16379d7f210ef1a28eac61ee851c179de6b6
Artem <artp80@gmail.com>
2022-10-24 16:39:48 +0400

    Fixed Header.jsx

--- react/src/components/header/Header.jsx
+++ react/src/components/header/Header.jsx
@@ -4,13 +4,12 @@ const tg = window.Telegram.WebApp
 
 
 const Header = () => {
-    console.log(tg.initDataUnsafe?.user)
     const onClose = ()=>{
         tg.close()
     }
     return (
         <div className="header">
-            <Button>Закрыть </Button>
+            <Button onClick={onClose}>Закрыть </Button>
             <span className="userName">{tg.initDataUnsafe?.user?.username}</span>
         </div>
     );
