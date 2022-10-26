d2bef2cf93622ca15ec17f159dfb1bad8ea740de
Artem <artp80@gmail.com>
2022-10-26 13:36:27 +0400

    Test Стили кнопки

--- bot/index.js
+++ bot/index.js
@@ -1,28 +1,33 @@
-import {validSecret} from "./utils/devops.js";
+import { validSecret } from "./utils/devops.js";
 import TelegramBot from "node-telegram-bot-api";
 
+const token = process.env["TOKEN"] || null;
+validSecret(token);
 
-const token = process.env["TOKEN"] || null
-validSecret(token)
+const bot = new TelegramBot(token, { polling: true });
+const wepAppUrl = "https://master--thunderous-souffle-b419f1.netlify.app";
 
-const bot = new TelegramBot(token, {polling: true});
-const wepAppUrl = "https://master--thunderous-souffle-b419f1.netlify.app/"
-
-bot.on('message', async (msg) => {
+bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
     await bot.sendMessage(chatId, `Ниже появится кнопка`, {
-        reply_markup : {
-keyboard:[[{text:"Заполнить форму"}]],
+        reply_markup: {
+            keyboard: [
+                [
+                    {
+                        text: "Заполнить форму",
+                        web_app: { url: wepAppUrl + "/form" },
+                    },
+                ],
+            ],
             one_time_keyboard: true,
-
         },
-
-
     });
 
     await bot.sendMessage(chatId, `Ниже появится кнопка`, {
-        reply_markup : {
-            inline_keyboard:[[{text:"DildoShop", web_app:{url:wepAppUrl}}]]
-        }
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: "DildoShop", web_app: { url: wepAppUrl } }],
+            ],
+        },
     });
 });
--- react/src/components/button/Button.jsx
+++ react/src/components/button/Button.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import cl from "./Button.module.css"
+import React from "react";
+import "./Button.module.css";
 const Button = (props) => {
-    console.table(cl)
-    return (
-        <button {...props} className={cl.value + props ? props.className : ""}></button>
-    );
+    return <button {...props} className={"button"}></button>;
 };
 
 export default Button;
