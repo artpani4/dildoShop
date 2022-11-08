1560d78471912d2839cd8eef45947aeb1a9955c9
Artem <artp80@gmail.com>
2022-11-08 23:06:12 +0400

    Добавил загрузку продуктов

--- react/src/App.js
+++ react/src/App.js
@@ -2,13 +2,12 @@ import "./App.css";
 import { useEffect } from "react";
 import Header from "./components/header/Header";
 import { useTelegram } from "./hooks/useTelegram";
-import Button from "./components/button/Button";
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/form/Form";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-    const { onToggleButton, tg } = useTelegram();
+    const { tg } = useTelegram();
     useEffect(() => {
         tg.ready();
     });
